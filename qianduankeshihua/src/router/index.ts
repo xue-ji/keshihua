08fea51ae586b6a5a67c2900dd77e41c4930210a
fengjinrong <fengjinrong522@gmail.com>
2023-12-19 21:08:14 +0800

    封装 axios

--- qianduankeshihua/src/router/index.ts
+++ qianduankeshihua/src/router/index.ts
@@ -6,6 +6,10 @@ const router = createRouter({
   routes: [
     {
       path: '/',
+      redirect: '/home',
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView
     },
@@ -19,5 +23,17 @@ const router = createRouter({
     }
   ]
 })
+
+// 路由前置守卫，验证用户身份，检查权限
+// to 即将进入目标路由对象，包含路径、参数、查询参数等
+// from 当前导航正要离开的路由对象，包含了当前路由的信息
+// next 函数，用于控制导航行为，他可以接收一个参数，用于指定导航的目标路由
+router.beforeEach((to, from, next) => {
+  // if (userlogin) {
+  //   next()
+  // } else{
+  //   next('/login')
+  // }
+});
 
 export default router
